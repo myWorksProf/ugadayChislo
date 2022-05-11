5b0ddeaaa7b4cb56cf75377dc89aa6c511a2b1d0
Алексей <78846964+myWorksProf@users.noreply.github.com>
2022-05-11 20:03:01 +0700

    Add files via upload
    Игра Угадай число. Начало.

--- ugadayChislo/src/ugadayChislo.java
+++ ugadayChislo/src/ugadayChislo.java
@@ -5,37 +5,29 @@ public class ugadayChislo {
         System.out.println("Игра УГАДАЙ ЧИСЛО.  Автор: Казаков Алексей, java224top");
         System.out.println("Ваша задача угадать загаданное мной число");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Что бы мне загадать число, ведите любое целое начальное число диапазана - ");
-        int min = scanner.nextInt();
-        System.out.print("Что бы мне загадать число, ведите любое целое конечное число диапазана - ");
-        int max = scanner.nextInt();
-        int rnd = rnd(min, max);
-        System.out.println("Я загадал число от " + min + " до " + max);
+        System.out.print("Что бы мне загадать число, ведите любое целое число - ");
+        int range = scanner.nextInt();
+        int number = (int) (Math.random() * range);
+        System.out.println("Я загадал число от 0 до " + range);
         System.out.print("Введи загаданное мной целое число = ");
         int yuorNamber = scanner.nextInt();
         firstBlock:
         {
-            for (int i = 0; i < 5; i++) {
-                if (yuorNamber < rnd) {
+            for (int i = 0; i < 3; i++) {
+                if (yuorNamber < number) {
                     System.out.println("Я загадал чило больше чем " + yuorNamber);
-                } else if (yuorNamber > rnd) {
+                } else if (yuorNamber > number) {
                     System.out.println("Я загадал число меньше чем " + yuorNamber);
                 } else {
                     System.out.println("* * *  УРА ТЫ ПОБЕДИЛ !!! * * *");
-                    System.out.println("Именно " + rnd + " я и загадал ! ");
+                    System.out.println("Именно " + number + " я и загадал ! ");
                     break firstBlock;
                 }
                 System.out.print("Введи загаданное мной целое число = ");
                 yuorNamber = scanner.nextInt();
             }
             System.out.println("Мне жаль но ты не угалал !!!");
-            System.out.println("Я загадал число - " + rnd);
+            System.out.println("Я загадал число - " + number);
         }
     }
-
-    public static int rnd(int min, int max) {
-        max -= min;
-        return (int) (Math.random() * ++max) + min;
-    }
-
 }
